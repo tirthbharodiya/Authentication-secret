a98afbb9cee196eed01d0788fd5572ce3b9c4100
Tirth <tirthpatel7620@gmail.com>
2022-06-13 16:13:33 +0530

    Second chnage

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const saltRound = 10;
 // const _ = require("lodash");
 // const date = require(__dirname + "/date.js"); //date.js requiring local file
 //date.getDate()  for use first function of date.js file
@@ -16,6 +22,16 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret:"This is session",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
@@ -23,11 +39,20 @@ email:String,
 password:String
 })
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret : secret, encryptedFields : ["password"]});
+userSchema.plugin(passportLocalMongoose);
+
+// const secret = process.env.SECRET; //Using Env file authentication
+
+// userSchema.plugin(encrypt,{secret : secret, encryptedFields : ["password"]}); //Plugin for encryption of password of schema.
 
 const User = new mongoose.model("User",userSchema);
 
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //For route requests
 app.route("/")
@@ -44,8 +69,11 @@ app.route("/register")
 })
 
 .post(function(req,res){
+    bcrypt.hash(req.body.password,saltRound,function(err,hash){
+
     const email = req.body.username;
-    const pass = req.body.password;
+    // const pass = md5(req.body.password); //Used Hashing method (md5) to hash the password 
+     const pass = hash; //   
 
 const newUser = new User({
 email:email,
@@ -61,6 +89,9 @@ newUser.save(function(err){
 });
 });
 
+    });
+    
+
 //for login route requests
 app.route("/login")
 
@@ -71,19 +102,26 @@ app.route("/login")
 .post((req,res)=>{
     const username = req.body.username;
     const pass = req.body.password;
+    // const pass = md5(req.body.password); //Used Hashing method to compare with hashed password in database
    User.findOne({email:username},function(err,foundUser){
     
 if(err){
-        res.send(err)
+        res.send(err);
     }else{
         if(foundUser){
-            if(foundUser.password === pass){
-                res.render("secrets");
-            }
+            bcrypt.compare(pass , foundUser.password , function(err,result){
+                console.log(err);
+                console.log(result);
+                if(result === true){
+                    res.render("secrets");
+                }
+            });
         }
     }
-   });
 });
+});
+// console.log(md5("hello"));
+            
 
 app.listen("3000",function(err){
     if(!err){
